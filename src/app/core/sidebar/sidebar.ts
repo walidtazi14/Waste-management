662ed3a43d9b10ac7bccaad27e41d0c1d600fa5a
walid <walid@dr-2.eu>
2026-02-25 06:47:00 -0500

    collapssable sidebar

--- src/app/core/sidebar/sidebar.ts
+++ src/app/core/sidebar/sidebar.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { NgClass, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, NgClass, NgIf],
   templateUrl: './sidebar.html'
 })
-export class Sidebar { }
+export class Sidebar {
+  isCollapsed = false;
+
+  toggleSidebar() {
+    this.isCollapsed = !this.isCollapsed;
+  }
+}
